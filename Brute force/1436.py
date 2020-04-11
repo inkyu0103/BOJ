8ce61ba6593f63142032d33595dec2c11b5c79da
inkyu <inkyu0103@gmail.com>
2020-04-11 20:03:55 +0900

    4.11

---
+++ Brute force/1436.py
@@ -0,0 +1,17 @@
+#1436 영화감독 숌
+
+user_input = int(input())
+num = 0
+counter = 0
+while(1):
+    if '666' in str(num):
+        counter += 1
+        num +=1
+
+        if counter == user_input:
+            print(num-1)
+            break;
+
+    else:
+        num +=1
+

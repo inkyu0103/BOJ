a34938f5fee949967847011619f8da62a6625949
inkyu <inkyu0103@gmail.com>
2020-07-09 15:32:10 +0900

    07.09

--- DFS , BFS/2667.py
+++ DFS , BFS/2667.py
@@ -1,49 +1,48 @@
-# 2667 단지번호 붙이기
+# 2667 단지 번호 붙이기
+
+import sys
+from collections import deque
+
+# (y,x)
+dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+
+def bfs (start):
+    count = 1
+    queue = deque()
+    queue.append(start)
+
+    while queue:
+        x,y = queue.popleft()
+        Maps[y][x] = 0
+
+        for move in dirs:
+            new_x , new_y = x+move[1] , y+move[0]
+
+            if 0<=new_x< len(Maps[0]) and 0 <=new_y<testCase and Maps[new_y][new_x]:
+                queue.append((new_x,new_y))
+                Maps[new_y][new_x] = 0
+                count += 1
+
+    count_box.append(count)
+    return 1
 
 
 
 
 
-# complex : 단지 수
-# count : 단지 내 아파트 개수
+testCase = int(sys.stdin.readline())
+group = 0
+count_box = []
 
-Vertex = int(input())
+# 하하 뿌듯하군요
+Maps=[[int(i) for i in sys.stdin.readline().rstrip('\n')] for _ in range(testCase)]
 
-Matrix = []
-Visit = []
-Stack = []
+for y in range(testCase):
+    for x in range(len(Maps[0])):
+        if Maps[y][x] == 1 :
+            group += bfs((x,y))
 
-count = 0
-complex = 0
-complex_num = []
-
-for i in range(Vertex):
-    Visit.append([0] * Vertex)
-    tmp = []
-    for i in input():
-        tmp.append(int(i))
-
-    Matrix.append(tmp)
-
-print(Matrix)
-print(Visit)
-
-
-while(1):
-
-    count = 0
-
-    Find()
-
-    val = find_start()
-
-    if val == False:
-        break
-    else:
-        continue
-
-
-print("Complex : " , complex)
-print("Complex Num : ", complex_num)
-#방문 결과는 잘 나온다.
-print(Visit)
+print(group)
+count_box.sort()
+for i in count_box:
+    print(i)

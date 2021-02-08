145b4cd94e79a5f98f6729eb6f863b446154dd46
Inkyu Kim <inkyu0103@gmail.com>
2021-02-08 21:37:16 +0900

    2.8

---
+++ DataStructure/2841.py
@@ -0,0 +1,34 @@
+# 2841
+
+# 자기보다 낮은 음이 들어오면 계속 손을 뗴야 한다.
+# 줄별로 스택을 따로 관리 해야겠다.
+import sys
+
+
+def sol():
+    N, P = map(int, sys.stdin.readline().split())
+    stack = [[] for i in range(N+1)]
+    answer = 0
+    while (1):
+        try:
+            line, plat = map(int,sys.stdin.readline().split())
+
+            if len(stack[line]) == 0 :
+                stack[line].append(plat)
+                answer += 1
+            else:
+                if stack[line][-1] > plat :
+                    while stack[line] and stack[line][-1] > plat:
+                        stack[line].pop()
+                        answer += 1
+
+                    # 스택이 빈 경우
+                    if len(stack[line]) == 0 :
+                        stack[line].append(plat)
+                        answer += 1
+
+                    elif stack[line] < plat :
+                        stack[line].append(plat)
+                        answer += 1
+        except:
+            return
--- DataStructure/9935.py
+++ DataStructure/9935.py
@@ -1,40 +1,38 @@
-#9935 문자열 폭발
+# 9935 문자열 폭발
+
 
 def sol():
-    myStr = input()
-    exploseStr = input()
-    # 마지막 단어가 들어올 때 확인하는건데 마지막 단어만 들어오는 경우가 최악의 케이스
-    checkStr = exploseStr[-1]
-    exploseStr = exploseStr[:-1]
-
-
+    userData = input()
+    boom = input()
+    textCheck = boom[-1]
+    textCheckLength = len(textCheck)
     stack = []
+    answer = ''
 
-    for i in range(len(myStr)):
-        if len(stack) == 0 :
-            stack.append(myStr[i])
+    for i in range(len(userData)):
+        if userData[i] not in  boom:
+            answer+=userData[i]
+        # 마지막 단어일 때
         else:
-            flag = 0
-            # 끝 문자이고, 스택의 길이가 더 길다면
-            if myStr[i] == checkStr and len(exploseStr) <= len(stack):
-                for i in range(len(exploseStr)):
-                    if stack[len(stack)-i-1] == exploseStr[len(exploseStr)-i-1]:
-                        continue
+            if userData[i] == textCheck:
+                try:
+                    # 정상적으로 다 있을 때
+                    if stack[-textCheck:] == boom[:-1]:
+                        for i in range(textCheckLength):
+                            stack.pop()
+                    # 같지 않을 때
                     else:
-                        # break를 표시하는 flag
-                        # 만약에 안 맞으면 얘도 추가해야함
-                        stack.append(checkStr)
-                        flag = 1
-                        break
-                if flag == 0 :
-                    for i in range(len(exploseStr)):
-                        stack.pop()
+                        for i in range(textCheckLength):
+                            answer += stack.pop()
+                        answer += textCheck
 
-            elif myStr[i] != checkStr:
-                stack.append(myStr[i])
-
-    if len(stack) == 0:
-        return "FRULA"
-    else:
-        return "".join(stack)
-print(sol())
+                # stack의 범위가 문제가 있을 때.
+                except:
+                    stacklen = len(stack)
+                    for i in range(stacklen):
+                        answer += stack.pop()
+                    answer += textCheck
+            else:
+                stack.append(userData[i])
+    print(answer)
+sol()

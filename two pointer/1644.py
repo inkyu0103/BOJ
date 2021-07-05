bd0c260c05ddced66294a5630caa57509dcaef6d
Inkyu Kim <inkyu0103@gmail.com>
2021-07-06 01:33:40 +0900

    7.5

--- two pointer/1644.py
+++ two pointer/1644.py
@@ -4,45 +4,50 @@ import sys
 import math
 input = sys.stdin.readline
 
+def isPrime(target):
+    if target <=1:
+        return False
+    if target % 2 == 0:
+        return True if target == 2 else False
+    for i in range(3,int(math.sqrt(target))+1,2):
+        if target % i == 0:
+            return False
+    return True
+
+
 def sol():
     N = int(input())
-
-    if N == 1 or N == 2:
+    if N == 1:
         print(0)
         return
 
-    left,right = 0,0
+    answer = 0
+    # 자기 자신이 소수일 경우
+    prime_arr = []
+    for target in range(N+1):
+        if isPrime(target):
+            prime_arr.append(target)
 
-    # 소수 배열 만들기
-    prime = [2,3,5,7]
-    for i in range(11,int(math.sqrt(N))+2):
-        print(prime)
-        flag = 0
-        if i % 2 == 0:
-            continue
-        for j in range(3,int(math.sqrt(i))+2):
-            if i % j == 0:
-                flag = 1
+    # left index,  right index, 누적 합
+    left,right= 0, 0
+    acc_sum = prime_arr[left]
+
+    # 먼저 right가 증가하고 acc_sum[right]에 들어가면 idx error 을 겪는 경우 존재.
+    while left <= right:
+        if acc_sum < N:
+            right += 1
+            if right < len(prime_arr):
+                acc_sum += prime_arr[right]
+            else:
                 break
-        if flag == 0:
-            prime.append(i)
-
-    tmp = prime[0]
-
-    count = 0
-    while 1:
-        if tmp >= N :
-            if tmp == N:
-                count += 1
-            tmp -= prime[left]
+        elif acc_sum > N :
+            acc_sum -= prime_arr[left]
+            left += 1
+        else:
+            answer += 1
+            acc_sum -= prime_arr[left]
             left += 1
 
-        elif right == len(prime)-1:
-            break
 
-        elif tmp < N :
-            right += 1
-            tmp += prime[right]
-    print(count)
-
+    print(answer)
 sol()

8263b9493bcb5fddd81f3da3a2d9baa48328b389
Inkyu Kim <inkyu0103@gmail.com>
2021-09-20 12:41:08 +0900

    2021.09.19:14916 solve

--- Dynamic Programming/14916.py
+++ Dynamic Programming/14916.py
@@ -1,15 +1,16 @@
 # 14916 거스름 돈
+import math
 n = int(input())
+INF = math.inf
 
-count = 0
+dp = [0]*(100001)
+dp[2],dp[4],dp[5] = 1,2,1
+for i in range(6,n+1):
+    a,b = math.inf,math.inf
+    if dp[i-2]:
+        a=dp[i-2]+1
+    if dp[i-5]:
+        b=dp[i-5]+1
+    dp[i] = min(a,b)
 
-while n > 0:
-    if n % 5 == 0:
-        print(n // 5 + count)
-        break
-
-    n -= 2
-    count += 1
-
-if n < 0:
-    print('-1')
+print(dp[n] if dp[n] else -1)

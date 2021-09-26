46525c77343d8446160e3b76fa945ad4603c5a35
Inkyu Kim <inkyu0103@gmail.com>
2021-09-26 14:48:03 +0900

    2021.09.26:9465 retry

--- Dynamic Programming/9465.py
+++ Dynamic Programming/9465.py
@@ -1,15 +1,18 @@
 # 9465
-
 tc = int(input())
 
 for _ in range(tc):
     length = int(input())
     value =[]
 
-
     #그래프
     for i in range(2):
         value.append(list(map(int,input().split())))
+
+    if length == 1:
+        print(max(value[0][0],value[1][0]))
+        continue
+
 
     value[0][1] += value[1][0]
     value[1][1] += value[0][0]
@@ -19,6 +22,12 @@ for _ in range(tc):
         value[1][i] += max(value[0][i-1],value[0][i-2])
 
     print(max(value[0][-1],value[1][-1]))
+
+
+'''
+runtime err: 길이가 1인 경우?
+
+'''
 
 
 

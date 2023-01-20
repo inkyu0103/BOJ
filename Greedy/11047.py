017fe3155566bc892a453887ea22136d8fbd32ed
Inkyu Kim <inkyu0103@gmail.com>
2023-01-21 01:34:51 +0900

    2023.01.21 update

--- Greedy/11047.py
+++ Greedy/11047.py
@@ -1,8 +1,8 @@
-#11047 동전 0
+# 11047 동전 0
 
 import sys
 
-Kinds , Cost = map(int,sys.stdin.readline().split())
+Kinds, Cost = map(int, sys.stdin.readline().split())
 coin = []
 startPoint = 0
 sum = 0
@@ -12,20 +12,19 @@ for _ in range(Kinds):
 coin.reverse()
 
 for i in range(Kinds):
-    if coin[i] < Cost :
+    if coin[i] < Cost:
         startPoint = i
         break
 
-while(1):
+while 1:
     if sum == Cost:
         print(count)
         break
 
     else:
-        if sum+coin[startPoint] > Cost:
+        if sum + coin[startPoint] > Cost:
             startPoint += 1
             continue
         else:
             sum += coin[startPoint]
             count += 1
-

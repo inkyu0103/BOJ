f9a0f12593c952671666edfbd46451ec7546181f
Inkyu Kim <inkyu0103@gmail.com>
2023-01-27 12:44:22 +0900

    2023.1.26 solved

--- Backtracking/15664.py
+++ Backtracking/15664.py
@@ -10,20 +10,20 @@ already = []
 visit = [0] * N
 
 
-def backtracking(count):
+def backtracking(count, idx):
     if count == M:
         print(*tmp)
         return
 
     last = -1
-    for i in range(N):
+    for i in range(idx, N):
         if not visit[i] and last != numbers[i]:
             visit[i] = 1
             last = numbers[i]
             tmp.append(numbers[i])
-            backtracking(count + 1)
+            backtracking(count + 1, i + 1)
             visit[i] = 0
             tmp.pop()
 
 
-backtracking(0)
+backtracking(0, 0)

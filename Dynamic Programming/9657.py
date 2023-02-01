1672a7d98b62836032ed27cd28ba2caad5c9b435
Inkyu Kim <inkyu0103@gmail.com>
2023-02-01 18:04:02 +0900

    2023.02.01 solved

--- Dynamic Programming/9657.py
+++ Dynamic Programming/9657.py
@@ -1,15 +1,23 @@
-# 돌 게임 3
+# 9657  돌 게임 3
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
+dp = [0] * 1002
 
-dp = [0]*(N+1)
-dp[1] = 1; dp[2] = 2;dp[3] = 1;dp[4]= 1
+# dp[i] : 이기는 사람
+dp[1], dp[2], dp[3], dp[4] = 0, 1, 0, 0
+# print(dp[:10])
 
-for i in range(1,N+1):
-    if not dp[i]:
-        dp[i] = min(dp[i-1]+1,dp[i-3]+1,dp[i-4]+1)
 
-print(dp[N])
-print("SK" if dp[N]%2 else "CY")
+for i in range(5, N + 1):
+    for j in [1, 3, 4]:
+        if dp[i - j] == 1:
+            dp[i] = 0
+            break
+
+        else:
+            dp[i] = 1
+
+print("CY" if dp[N] else "SK")

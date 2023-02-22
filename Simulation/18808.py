282a85e975611c856d14b9a45f56fd0e7761b29c
Inkyu Kim <inkyu0103@gmail.com>
2023-02-22 13:52:05 +0900

    2023.02.22 18808 solved

--- Simulation/18808.py
+++ Simulation/18808.py
@@ -3,23 +3,60 @@ import sys
 input = sys.stdin.readline
 
 
-def rotate(sticker):
-    sticker_row, sticker_col = len(sticker), len(sticker[0])
-    rotate_sticker = [[0] * sticker_row for _ in range(sticker_col)]
+def rotate():
+    rotate_sticker = [[0] * R for _ in range(C)]
 
-    for r in range(sticker_row):
-        for c in range(sticker_col):
-            rotate_sticker[c][sticker_row - r - 1] = sticker[r][c]
+    for r in range(R):
+        for c in range(C):
+            rotate_sticker[c][R - r - 1] = sticker[r][c]
 
     return rotate_sticker
 
 
+def attachable(start_r, start_c):
+    # 스티커가 붙을 수 있는지 확인
+    for r in range(R):
+        for c in range(C):
+            if result[start_r + r][start_c + c] == 1 and sticker[r][c]:
+                return False
+
+    # 붙을 수 있다면? 붙인다
+    for r in range(R):
+        for c in range(C):
+            if sticker[r][c]:
+                result[start_r + r][start_c + c] = 1
+    # 붙일 수 있음을 알려줌
+    return True
+
+
 N, M, K = map(int, input().split())
 result = [[0] * M for _ in range(N)]
+answer = 0
 
 for _ in range(K):
     R, C = map(int, input().split())
-    g = [list(map(int, input().split())) for _ in range(R)]
-    is_rotate_essential = False
+    sticker = [list(map(int, input().split())) for _ in range(R)]
 
-    is_stick = True
+    for _ in range(4):
+        is_attachable = False
+
+        for r in range(N - R + 1):
+            if is_attachable:
+                break
+            for c in range(M - C + 1):
+                if attachable(r, c):
+                    is_attachable = True
+                    break
+
+        if is_attachable:
+            break
+        sticker = rotate()
+        R, C = C, R
+
+for r in range(N):
+    for c in range(M):
+        # 오호 이런거 좋다.
+        answer += result[r][c]
+
+
+print(answer)

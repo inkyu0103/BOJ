adcee13e0793eec8a0e456722514960fc4eb04fe
Inkyu Kim <inkyu0103@gmail.com>
2023-03-24 14:53:48 +0900

    2023.03.24 16985 retry

--- Simulation/16985.py
+++ Simulation/16985.py
@@ -3,7 +3,7 @@
 import sys
 import copy
 from math import inf
-from itertools import product
+from itertools import product, permutations
 from collections import deque
 
 
@@ -11,7 +11,7 @@ def rotate(board):
     result = copy.deepcopy(board)
     for r in range(5):
         for c in range(5):
-            result[r][c] = board[c][5 - r - 1]
+            result[c][5 - r - 1] = board[r][c]
     return result
 
 
@@ -35,9 +35,9 @@ def bfs(maze):
         [[0, 4, 4], [4, 0, 0]],
     ]
     tmp_answer = inf
+
     for _in, _out in in_out:
-        bb
-        if is_in_out_open(_in, _out, maze):
+        if not is_in_out_open(_in, _out, maze):
             continue
 
         visit = [[[0] * 5 for r in range(5)] for h in range(5)]
@@ -45,6 +45,7 @@ def bfs(maze):
 
         q = deque()
         q.append(_in)
+        visit[_in[0]][_in[1]][_in[2]] = 1
 
         while q:
             cur_h, cur_r, cur_c = q.popleft()
@@ -53,8 +54,6 @@ def bfs(maze):
                 new_h, new_r, new_c = cur_h + dh, cur_r + dr, cur_c + dc
 
                 if new_h == _out[0] and new_r == _out[1] and new_c == _out[2]:
-
-                    # 만약 도착지라면, tmp_answer과 비교 한 후 도착
                     tmp_answer = min(tmp_answer, visit[cur_h][cur_r][cur_c] + 1)
                     is_reach = True
                     break
@@ -67,7 +66,7 @@ def bfs(maze):
                     visit[new_h][new_r][new_c] = visit[cur_h][cur_r][cur_c] + 1
                     q.append([new_h, new_r, new_c])
 
-            if is_reach:
+            if is_reach or tmp_answer == 12:
                 break
 
     return tmp_answer
@@ -76,7 +75,7 @@ def bfs(maze):
 input = sys.stdin.readline
 boards = [[] for _ in range(5)]
 
-case = list(product([0, 1, 2, 3], repeat=4))
+case = list(product([0, 1, 2, 3], repeat=5))
 answer = inf
 
 
@@ -89,8 +88,28 @@ for _ in range(5):
         boards[_].append(rotate(boards[_][i - 1]))
 
 
-for floor in case:
-    maze = [boards[i][floor[i]] for _ in range(5)]
-    answer = min(answer, bfs(maze))
+for f in permutations([0, 1, 2, 3, 4], 5):
+    rotated_boards = [
+        boards[f[0]],
+        boards[f[1]],
+        boards[f[2]],
+        boards[f[3]],
+        boards[f[4]],
+    ]
 
-print(answer if answer != inf else -1)
+    for floor in case:
+        maze = [
+            rotated_boards[0][floor[0]],
+            rotated_boards[1][floor[1]],
+            rotated_boards[2][floor[2]],
+            rotated_boards[3][floor[3]],
+            rotated_boards[4][floor[4]],
+        ]
+
+        answer = min(answer, bfs(maze))
+
+
+print(answer - 1 if answer != inf else -1)
+"""
+와 판을 쌓는 순서 고려!
+"""

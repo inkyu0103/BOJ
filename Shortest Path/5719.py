dad555cc9c409f0f1ec659c6498b3bfbf48484b6
Inkyu Kim <inkyu0103@gmail.com>
2023-04-04 13:48:21 +0900

    2023.04.04 solved

--- Shortest Path/5719.py
+++ Shortest Path/5719.py
@@ -1,6 +1,7 @@
 # 거의 최단경로
 import sys
 from math import inf
+from collections import defaultdict, deque
 import heapq
 
 input = sys.stdin.readline
@@ -18,37 +19,30 @@ def solve():
 
         for nc, nn in adj[cn]:
 
-            if nc + cc >= dist[nn]:
+            if nc + cc >= dist[nn] or nn in exclude_path[cn]:
                 continue
 
             dist[nn] = nc + cc
             heapq.heappush(q, [nc + cc, nn])
 
-    return dist[D]
+    return dist
 
 
-def solve2(min_dist):
-    dist = [inf] * N
-    dist[S] = 0
-    q = [[0, S]]
+def solve2():
+    q = deque()
+    q.append(D)
 
     while q:
-        cc, cn = heapq.heappop(q)
-        if dist[cn] != cc:
+        cur = q.popleft()
+        if cur == S:
             continue
 
-        for nc, nn in adj[cn]:
+        for cost, nxt_node in rev_adj[cur]:
 
-            if nn == D and nc + cc <= min_dist:
-                continue
-
-            if nc + cc >= dist[nn]:
-                continue
-
-            dist[nn] = nc + cc
-            heapq.heappush(q, [nc + cc, nn])
-
-    return dist[D]
+            if dist[cur] == dist[nxt_node] + cost:
+                exclude_path[nxt_node].add(cur)
+                q.append(nxt_node)
+    return exclude_path
 
 
 while True:
@@ -60,11 +54,16 @@ while True:
     S, D = map(int, input().split())
 
     adj = [[] for _ in range(N)]
+    rev_adj = [[] for _ in range(N)]
 
     for _ in range(M):
         U, V, P = map(int, input().split())
         adj[U].append([P, V])
+        rev_adj[V].append([P, U])
 
-    min_dist = solve()
-    answer = solve2(min_dist)
-    print(-1 if answer == inf else answer)
+    exclude_path = defaultdict(set)
+    dist = solve()
+    exclude_path = solve2()
+    answer = solve()
+
+    print(-1 if answer[D] == inf else answer[D])

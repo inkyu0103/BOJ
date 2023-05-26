6e0d456e143f22f54b7bfbe2ba8195b7fd07d121
Inkyu Kim <inkyu0103@gmail.com>
2023-05-26 18:03:09 +0900

    2023.05.26 1781 solved

--- DataStructure/1781.py
+++ DataStructure/1781.py
@@ -7,17 +7,24 @@ input = sys.stdin.readline
 
 def sol():
     N = int(input())
-    data = []
+    count_deadline = [[] for _ in range(200002)]
+    q = []
+    answer = 0
 
     for _ in range(N):
         d, c = map(int, input().split())
-        heapq.heappush(data, (d, -c))
+        count_deadline[d].append(-c)
 
-    print(data)
-    print(heapq.heappop(data))
-    print(data)
+    for d in range(200001, 0, -1):
+        for c in count_deadline[d]:
+            heapq.heappush(q, c)
 
-    stack = []
+        if not q:
+            continue
+        answer -= q[0]
+        heapq.heappop(q)
+
+    print(answer)
 
 
 sol()
